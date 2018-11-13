f54cdc6f65be5322a0381b07507a85e8601d62ed
JoannaWilk <wielki.wilq@gmail.com>
2018-11-13 16:44:28 +0100

    add view for game details and a template

--- bgorganizer/settings.py
+++ bgorganizer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'games.apps.GamesConfig'
+    'games.apps.GamesConfig',
 ]
 
 MIDDLEWARE = [
--- games/views.py
+++ games/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Game
+
+
+class DetailView(generic.DetailView):
+    model = Game
+    template_name = 'games/detail.html'
